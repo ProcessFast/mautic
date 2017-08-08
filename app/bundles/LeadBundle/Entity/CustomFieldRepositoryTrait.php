014d7a00b3447d73747b5e703e53959e478078f1
John Linhart <admin@escope.cz>
2017-08-08 09:49:09 +0200

    Boolean custom fields; consider null to be unknown value, not false

--- app/bundles/LeadBundle/Entity/CustomFieldRepositoryTrait.php
+++ app/bundles/LeadBundle/Entity/CustomFieldRepositoryTrait.php
@@ -303,15 +303,16 @@ trait CustomFieldRepositoryTrait
 
         //loop over results to put fields in something that can be assigned to the entities
         foreach ($values as $k => $r) {
+            switch ($fields[$k]['type']) {
+                case 'number':
+                    $r = (float) $r;
+                    break;
+                case 'boolean':
+                    $r = is_null($r) ? $r : (bool) $r;
+                    break;
+            }
+
             if (isset($fields[$k])) {
-                switch ($fields[$k]['type']) {
-                    case 'number':
-                        $r = (float) $r;
-                        break;
-                    case 'boolean':
-                        $r = (int) $r;
-                        break;
-                }
                 if ($byGroup) {
                     $fieldValues[$fields[$k]['group']][$fields[$k]['alias']]          = $fields[$k];
                     $fieldValues[$fields[$k]['group']][$fields[$k]['alias']]['value'] = $r;
