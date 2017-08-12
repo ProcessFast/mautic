5ebdba6f2b4d7960067ff6230623804581abb922
Alan Hartless <alan@devkardia.com>
2017-08-12 09:51:49 -0500

    Added a regex validation on the segment filter

--- app/bundles/LeadBundle/Config/config.php
+++ app/bundles/LeadBundle/Config/config.php
@@ -439,9 +439,14 @@ return [
                 'alias'     => 'leadlist_choices',
             ],
             'mautic.form.type.leadlist_filter' => [
-                'class'     => 'Mautic\LeadBundle\Form\Type\FilterType',
-                'alias'     => 'leadlist_filter',
-                'arguments' => ['mautic.factory'],
+                'class'       => 'Mautic\LeadBundle\Form\Type\FilterType',
+                'alias'       => 'leadlist_filter',
+                'arguments'   => ['mautic.factory'],
+                'methodCalls' => [
+                    'setConnection' => [
+                        'database_connection',
+                    ],
+                ],
             ],
             'mautic.form.type.leadfield' => [
                 'class'     => 'Mautic\LeadBundle\Form\Type\FieldType',
